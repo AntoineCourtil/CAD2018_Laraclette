3f7d4e3aae69c3fa024c1372ed4a936dad1409be
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2018-04-05 16:50:05 +0200

    Importation du package engine et game

--- src/bataillenavale/Main.java
+++ src/bataillenavale/Main.java
@@ -1,7 +1,25 @@
 package bataillenavale;
 
+import bataillenavale.game.Painter;
+import bataillenavale.engine.GameEngineGraphical;
+import bataillenavale.game.Controller;
+import bataillenavale.game.Game;
+
+/**
+ * lancement du moteur avec le jeu
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// creation du jeu particulier et de son afficheur
+		Game game = new Game();
+		Painter painter = new Painter();
+		Controller controller = new Controller();
+
+		// classe qui lance le moteur de jeu generique
+		GameEngineGraphical engine = new GameEngineGraphical(game, painter, controller);
+		engine.run();
+	}
+
 }
