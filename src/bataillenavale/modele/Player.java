f843688837478174fbe61aa6e946e10f21585080
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2018-04-05 19:53:58 +0200

    Classe player avec les chooseBoat

--- src/bataillenavale/modele/Player.java
+++ src/bataillenavale/modele/Player.java
@@ -5,6 +5,7 @@ import bataillenavale.boatFactory.abstractBoat.Bateau;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Player implements Serializable {
 
@@ -17,8 +18,7 @@ public class Player implements Serializable {
     private List<Point2D> tirsEchoues;
 
 
-
-    public Player(ArrayList<Bateau> boatArrayList){
+    public Player(ArrayList<Bateau> boatArrayList) {
 
         boatList = boatArrayList;
         tirsEchoues = new ArrayList<>();
@@ -28,70 +28,136 @@ public class Player implements Serializable {
 
     }
 
-    public void shootIA(){
+    /**
+     * Place tous les boats du joueur aléatoirement, dans une position valide
+     */
+    public void placeBoatAlea(){
+        Random rand = new Random();
+        //TODO A TESTER
+        int x = 0;
+        int y = 0;
+        int orientationAlea = 0;
+        int index = 0;
+        Bateau.Orientation[] orientations = {Bateau.Orientation.EST, Bateau.Orientation.NORD, Bateau.Orientation.SUD, Bateau.Orientation.OUEST};
+        for(Bateau bateau : boatList){
+            boolean isValide = false;
+            while(!isValide) {
+                x = rand.nextInt(BatailleNavale.WIDTH - 1);
+                y = rand.nextInt(BatailleNavale.HEIGHT - 1);
+                orientationAlea = rand.nextInt(3);
+                bateau.setOrientation(orientations[orientationAlea]);
+                bateau.setPosition(new Point2D(x, y));
+                for(int i = 0; i<index; i++){
+                    Bateau verifBoat = boatList.get(i);
+                    //DetectBoat dit s'il trouve un boat ou non, s'il trouve un boat c'est pas valide.
+                    isValide = !detectBoat(verifBoat, new Point2D(x, y));
+                }
+            }
+            index ++;
+        }
+
+    }
+
+    public void shootIA() {
         getStrategie().generateShoot(tirsEchoues);
     }
 
-    public boolean receiveShoot(Point2D pos, int damage){
+    public boolean receiveShoot(Point2D pos, int damage) {
 
-        for(Bateau bateau : boatList){
-
-            if(bateau.getHP() > 0) {
-                Point2D baseBateau = bateau.getPosition();
-                Bateau.Orientation orientation = bateau.getOrientation();
-                int size = bateau.getSize();
-
-                for(int i = 0; i<size; i++){
-
-                    switch (orientation){
-
-                        case NORD:
-
-//                            if(pos == new Point2D(baseBateau.getX() + i, baseBateau.getY()){
-//
-//                            }
-
-                            break;
-
-                        case SUD:
-                            break;
-
-                        case OUEST:
-                            break;
-
-                        case EST:
-                            break;
-
-                    }
-
+        boolean trouve = false;
+        for (Bateau bateau : boatList) {
+            if (bateau.getHP() > 0) {
+                trouve = detectBoat(bateau, pos);
+                if (trouve) {
+                    bateau.setHP(bateau.getHP() - damage);
                 }
-
             }
 
         }
-
-        return false;
-
-    }
-
-    public boolean chooseBoat(Point2D pos){
-
-        return false;
+        return trouve;
 
     }
 
-    public int getBoatIndexFromPos(Point2D pos){
+    public boolean chooseBoat(Point2D pos) {
 
-        return 0;
+        boolean trouve = false;
+        int index = 0;
+        for (Bateau bateau : boatList) {
+            if (bateau.getHP() > 0) {
+                trouve = detectBoat(bateau, pos);
+                if (trouve) {
+                    currentBoatIndex = index;
+                }
+            }
+            index ++;
+
+        }
+        return trouve;
+
+    }
+
+    /**
+     * @param pos la position qu'on veut check s'il y a un bateau ou non
+     * @return -1 si pas de bateau sinon l'index du bateau dans la list listboat
+     */
+    public int getBoatIndexFromPos(Point2D pos) {
+        int index = 0;
+        boolean trouve = false;
+        for (Bateau bateau : boatList) {
+            trouve = detectBoat(bateau, pos);
+            if (trouve) break;
+            index++;
+        }
+        if (trouve) return index;
+        else return -1;
+    }
+
+    private boolean detectBoat(Bateau bateau, Point2D pos) {
+        boolean trouve = false;
+        Bateau.Orientation orientation = bateau.getOrientation();
+        int size = bateau.getSize();
+        int x = bateau.getPosition().getX();
+        int y = bateau.getPosition().getY();
+
+        for (int i = 0; i < size; i++) {
+            if(x >= BatailleNavale.WIDTH || y >= BatailleNavale.HEIGHT || x<0 || y<0){
+                //On sort du plateau donc on simule un bateau trouve afin de dire que la case est non valide
+                trouve = true;
+                break;
+            }
+            switch (orientation) {
+                case NORD:
+                    if (pos.equals(new Point2D(x, y))) trouve = true;
+                    else y--;
+                    break;
+                case SUD:
+                    if (pos.equals(new Point2D(x, y))) trouve = true;
+                    else y++;
+                    break;
+
+                case OUEST:
+                    if (pos.equals(new Point2D(x, y))) trouve = true;
+                    else x++;
+                    break;
+
+                case EST:
+                    if (pos.equals(new Point2D(x, y))) trouve = true;
+                    else x--;
+                    break;
+            }
+
+            if (trouve) break;
+        }
+        return trouve;
 
     }
 
 
-    public Bateau getCurrentBoat(){
+    public Bateau getCurrentBoat() {
         return boatList.get(currentBoatIndex);
     }
 
-    public void addFailedShoot(Point2D pos){
+    public void addFailedShoot(Point2D pos) {
         tirsEchoues.add(pos);
     }
 
@@ -101,7 +167,7 @@ public class Player implements Serializable {
         return strategie;
     }
 
-    public void setStrategie(Strategie s){
+    public void setStrategie(Strategie s) {
         strategie = s;
     }
 
