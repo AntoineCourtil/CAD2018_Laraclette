defd2c23f0abd783b5263ae350aa912cbce5db75
Guillaume Petit <guillaume.petit4@etu.univ-lorraine.fr>
2018-04-06 13:27:12 +0200

    Ajout image factory et background (pas encore affiché)

---
+++ src/bataillenavale/game/ImageFactory.java
@@ -0,0 +1,32 @@
+package bataillenavale.game;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageFactory {
+
+    private static ImageFactory instance;
+
+    private BufferedImage background;
+
+    private ImageFactory() {
+        loadBackground()
+    }
+
+    public static ImageFactory getInstance() {
+        if (instance == null) instance = new ImageFactory();
+        return instance;
+    }
+
+    private void loadBackground() {
+        try {
+            background = ImageIO.read(new File("res/img/background.jpg"));
+        } catch (IOException ioe) {
+            System.out.println("Impossible de charger une des image : ");
+            System.out.println(ioe.getMessage());
+            System.exit(-1);
+        }
+    }
+}
